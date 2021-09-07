13cf00404e77e22b6194b2826582627830195962
Gaston <gaston@github.com>
2021-09-07 23:21:31 +0200

    js algorithms Is Prime

---
+++ primeNumber.js
@@ -0,0 +1,21 @@
+// Determine wether on input number is a prime number and
+// Algorithm needs to do one thing: 
+// (1) Try dividing the number by all smaller numbers and return true
+// if it's only divisible by itself and 1 
+
+function isPrime(n) {
+    for (let i = 2; i < n ; i++) { // 1
+        console.log('Running') 
+        if(n % i === 0) { // n 
+            return false; // 1
+        } 
+    }
+    return true; // 1
+}
+
+// Best Case: nubmer = 1 OR number = 2 => O(1)
+// Average Case: O(n) = 1
+// Worst Case: number = 27,277 => O(n)
+
+console.log(isPrime(5))
+console.log(isPrime(9))
